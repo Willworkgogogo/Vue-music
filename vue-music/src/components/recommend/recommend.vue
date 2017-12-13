cbe77ce1ccc164130d8655ce216855d15ed2d0af
wangweiwei <will_workhard@163.com>
2017-12-14 01:32:51 +0800

    新增头部和tab五个组件

---
+++ vue-music/src/components/recommend/recommend.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="recommend">
+    recommend
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
