99cd43e2c3589f83c5d846faf87020d6b9f49338
wangweiwei <will_workhard@163.com>
2017-12-27 00:35:38 +0800

    func _initSliderWidth

--- vue-music/src/base/slider/slider.vue
+++ vue-music/src/base/slider/slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
       <slot></slot>
     </div>
     <div class="dots">
@@ -10,6 +10,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {addClass} from 'common/js/dom'
+
   export default {
     props: {
       loop: {
@@ -24,6 +26,38 @@
         type: Number,
         default: 4000
       }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._initSliderWidth()
+        this._initSlider()
+      }, 20)
+    },
+    methods: {
+      _initSliderWidth() {
+        this.children = this.$ref.sliderGroup.children
+
+        let width = 0
+        let sliderWidth = this.$ref.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+
+          child.style.width = sliderWidth + 'px'
+          // 父容器的宽度 累加
+          width += sliderWidth
+        }
+
+        if (this.loop) {
+          // 循环轮播的时候， 需要前后复制两个slider的宽度
+          width += 2 * sliderWidth
+        }
+
+        this.$ref.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+
+      }
     }
   }
 </script>
@@ -37,4 +71,36 @@
       position: relative
       overflow: hidden
       white-space: nowrap
+      .slider-item
+        float: left
+        box-sizing: border-box
+        overflow: hidden
+        text-align: center
+        a
+          display: block
+          width: 100%
+          overflow: hidden
+          text-decoration: none
+        img
+          display: block
+          width: 100%
+    .dots
+      position: absolute
+      right: 0
+      left: 0
+      bottom: 12px
+      transform: translateZ(1px)
+      text-align: center
+      font-size: 0
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+        &.active
+          width: 20px
+          border-radius: 5px
+          background: $color-text-ll
 </style>
