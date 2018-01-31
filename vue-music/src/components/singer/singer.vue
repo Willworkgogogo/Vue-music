c0faef747a7113c30206eeeb7e26fc42fdd23a28
wangweiwei <will_workhard@163.com>
2018-02-01 00:18:05 +0800

    [新增] - 歌手页面

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -5,8 +5,62 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
 
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10 // 自定义取前10条为热门
+
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+      this._normalizeSinger(this.singers)
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log('singer..', this.singers)
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        console.log('map', map)
+      }
+    }
   }
 </script>
 
