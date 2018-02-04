38f02a7acd3ba9c7370f51e6b5c16cb37df8cf40
wangweiwei <will_workhard@163.com>
2018-02-05 00:20:49 +0800

    添加歌手列表页右侧滚动效果

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singer">
-    singer
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
@@ -8,6 +8,7 @@
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
 
   const HOT_NAME = '热门'
   const HOT_SINGER_LEN = 10 // 自定义取前10条为热门
@@ -20,14 +21,13 @@
     },
     created() {
       this._getSingerList()
-      this._normalizeSinger(this.singers)
     },
     methods: {
       _getSingerList() {
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
-            this.singers = res.data.list
-            console.log('singer..', this.singers)
+            this.singers = this._normalizeSinger(res.data.list)
+            console.log('singers: ', this.singers)
           }
         })
       },
@@ -39,6 +39,7 @@
           }
         }
         list.forEach((item, index) => {
+          // 填充热门数据
           if (index < HOT_SINGER_LEN) {
             map.hot.items.push(new Singer({
               id: item.Fsinger_mid,
@@ -58,12 +59,35 @@
             name: item.Fsinger_name
           }))
         })
-        console.log('map', map)
+
+        // 做有序列表处理
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+
+        return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
