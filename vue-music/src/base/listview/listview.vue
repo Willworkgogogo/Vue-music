5d672d7862490776183b065f1ade11b50e93a1a1
wangweiwei <will_workhard@163.com>
2018-02-07 00:48:15 +0800

    title吸顶、边界限制

--- vue-music/src/base/listview/listview.vue
+++ vue-music/src/base/listview/listview.vue
@@ -18,8 +18,15 @@
     </ul>
     <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
       <ul>
-        <li class="item" v-for="(item, index) in shortcutList" :data-index="index">{{item}}</li>
+        <li class="item"
+            v-for="(item, index) in shortcutList"
+            :data-index="index"
+            :class="{'current': currentIndex === index}"
+        >{{item}}</li>
       </ul>
+    </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
     </div>
   </scroll>
 </template>
@@ -29,6 +36,7 @@
   import {getData} from 'common/js/dom'
 
   const ANCHOR_HEIGHT = 18
+  const TITLE_HEIGHT = 30
 
   export default {
     created() {
@@ -45,7 +53,8 @@
     data() {
       return {
         scrollY: -1,
-        currentIndex: 0 // 高亮下标
+        currentIndex: 0, // 高亮下标
+        diff: 0
       }
     },
     computed: {
@@ -53,6 +62,10 @@
         return this.data.map((group) => {
           return group.title.substring(0, 1)
         })
+      },
+      fixedTitle() {
+        if (this.scrollY > 0) return ''
+        return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
       }
     },
     methods: {
@@ -75,6 +88,14 @@
         this.scrollY = pos.y
       },
       _scrollTo(index) {
+        if (!index && index !== 0) return
+        if (index < 0) {
+          index = 0
+        } else if (index > this.listHeight.length - 2) {
+          index = this.listHeight.length - 2
+        }
+        // 手动触发scroll方法，否则点击不会有active效果
+        this.scrollY = -this.listHeight[index]
         this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
       },
       _calculateHeight() {
@@ -98,15 +119,34 @@
       },
       scrollY(newY) {
         const listHeight = this.listHeight
-        for (let i = 0; i < listHeight.length; i++) {
+        // 当滚动到顶部，newY > 0
+        if (newY > 0) {
+          this.currentIndex = 0
+          return
+        }
+        // 中间部分滚动
+        for (let i = 0; i < listHeight.length - 1; i++) {
           let height1 = listHeight[i]
           let height2 = listHeight[i + 1]
-          if (!height2 || (-newY > height1 && -newY < height2)) {
+          if (-newY >= height1 && -newY < height2) {
             this.currentIndex = i
+            // 用于计算每个区块上限 跟 已滚动距离之差，用于watch跟fixed块的距离
+            // 注意newY为负值
+            this.diff = height2 + newY
             return
           }
         }
-        this.currentIndex = 0
+        // 当滚动到底部
+        this.currentIndex = listHeight.length - 2
+      },
+      diff(newVal) {
+        let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+        if (this.fixedTop === fixedTop) {
+          return
+        }
+        this.fixedTop = fixedTop
+        console.log(this.fixedTop)
+        this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
       }
     },
     components: {
